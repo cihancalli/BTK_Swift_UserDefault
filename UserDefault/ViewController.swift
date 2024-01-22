40f9ff5119f172b08310b57fd9416753cf7609cb
Cihan Çallı <cihan.callii@gmail.com>
2024-01-22 12:13:36 +0300

    Initial Commit

---
+++ UserDefault/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefault
+//
+//  Created by Cihan on 22.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
